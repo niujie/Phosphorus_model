d93a5de244d6f879a6205955a344fb0c0acdb6f5
U-MSU-PHANI\Administrator <Administrator@msu-phani.(none)>
2013-10-02 16:57:43 -0400

    done

--- rainfall.m
+++ rainfall.m
@@ -1,20 +1,22 @@
 function p = rainfall(t)
 
 p = 0.0;
+n   = 0.625;          % Dimensionless, porosity
+Zr  = 100;            % unit: cm, soil depth
 date = datestr(t + datenum('01-Jan-1998'));
 month = date(4:6);
 switch month
     case {'May', 'Jun', 'Jul', 'Aug', 'Sep'}
         times = poissrnd(0.46);
         for i = 1 : times
-            p = p + exprnd(1.26);
+            p = p + exprnd(1.26/(n*Zr))*(n*Zr);
         end
 %         pd1 = makedist('Poisson', 'lambda', 0.46);
 %         pd2 = makedist('Exponential', 'mu', 1.26);
     otherwise
         times = poissrnd(0.05);
         for i = 1 : times
-            p = p + exprnd(0.4);
+            p = p + exprnd(0.4/(n*Zr))*(n*Zr);
         end
 %         pd1 = makedist('Poisson', 'lambda', 0.05);
 %         pd2 = makedist('Exponential', 'mu', 0.4);
